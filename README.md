7e5046081b451d0d42df1a7f9656534bffcbc9da
Mark Notton <mark@yellostudio.co.uk>
2016-08-08 16:35:22 +0100

    Init Commit.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# craft-plugin-themer
-Customise the colour preferences of the clientside aestetics of the CMS.
+# Themer *for Craft CMS*
+Customise the colour preferences of the clientside aesthetics of the CMS.
+
+## TODO:
+- Add better colour picker
+- Update UI and make it responsive
+- Add option to include image background
+- Add a collection premade themes
---
+++ themer/ThemerPlugin.php
@@ -0,0 +1,65 @@
+<?php
+namespace Craft;
+
+class ThemerPlugin extends BasePlugin {
+  public function getName() {
+    return Craft::t('Themer');
+  }
+
+  public function getVersion() {
+    return '0.1';
+  }
+
+  public function getDeveloper() {
+    return 'Yello Studio';
+  }
+
+  public function getDeveloperUrl() {
+    return 'http://yellostudio.co.uk';
+  }
+
+  public function getDocumentationUrl() {
+    return 'https://github.com/marknotton/craft-plugin-themer';
+  }
+
+  public function getReleaseFeedUrl() {
+    return 'https://raw.githubusercontent.com/marknotton/craft-plugin-themer/master/themer/releases.json';
+  }
+
+  protected function defineSettings() {
+    return array(
+      'header'     => array(AttributeType::String, 'default' => '#333F4D'),
+      'background' => array(AttributeType::String, 'default' => '#FFFFFF'),
+      'buttons'    => array(AttributeType::String, 'default' => '#DA5A47'),
+      'headings'   => array(AttributeType::String, 'default' => '#DA5A47'),
+      'links'      => array(AttributeType::String, 'default' => '#0d78f2'),
+      'sidebar'    => array(AttributeType::String, 'default' => '#737F8C'),
+      'pane'       => array(AttributeType::String, 'default' => '#F4F5F6'),
+      'switch'     => array(AttributeType::String, 'default' => '#00B007'),
+      'text'       => array(AttributeType::String, 'default' => '#29323d'),
+      'help'       => array(AttributeType::String, 'default' => '#9d2dcc'),
+      'small'      => array(AttributeType::String, 'default' => '#000000')
+    );
+  }
+
+  public function getSettingsHtml() {
+    return craft()->templates->render('themer/settings', array(
+      'settings' => $this->getSettings()
+    ));
+  }
+
+  public function init() {
+    if ( craft()->request->isCpRequest())  {
+
+      return craft()->templates->render('themer/css', array(
+        'settings' => $this->getSettings()
+      ));
+
+      if( !craft()->userSession->isLoggedIn() ) {
+        craft()->templates->includeJs("$('body').addClass('login')");
+      }
+    }
+  }
+
+
+}
